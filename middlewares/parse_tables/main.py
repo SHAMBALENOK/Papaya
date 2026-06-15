f719b443e1fd0dc3f5fcc20a751e971a04505873
Keiby <166421956+SHAMBALENOK@users.noreply.github.com>
2026-06-15 09:29:22 +0200

    0.4.5.3.1
    bugfix

--- middlewares/parse_tables/main.py
+++ middlewares/parse_tables/main.py
@@ -1,5 +1,5 @@
-import pdf_processing as pdf
-import sql_processing as sql
+from . import pdf_processing as pdf
+from . import sql_processing as sql
 
 def pdf_to_db(pdfname):
     sql.tabulate(pdf.extract_data(pdfname))
